a943fcbb981bd8411cc9afc7e136d791cd93acbd
benedwards23 <benedwards@Bens-MacBook-Air.local>
2017-03-28 21:01:23 +0100

    First working product

--- myScript.js
+++ myScript.js
@@ -1,3 +1,48 @@
-function calculateFunction() {
-   alert('This function works.')
-   }
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+function getValues()
+{
+//Assume form with id="theform"
+var theForm = document.forms["JourneyCost"];
+
+
+//Get a reference to the TextBox
+var distance = theForm.elements["distance"];
+var mpg = theForm.elements["mpg"];
+var petrolPrice = theForm.elements["fuelCost"]
+
+
+var miles = 0;
+var milesPerGallon = 0;
+var price = 0;
+
+
+//If the textbox is not blank
+if(distance.value!="")
+{
+miles = parseFloat(distance.value);
+}
+
+if(mpg.value!="")
+{
+milesPerGallon = parseFloat(mpg.value);
+}
+
+if(petrolPrice.value!="")
+{
+price = parseFloat(petrolPrice.value);
+}
+
+var numberOfGallonsUsed = (miles / milesPerGallon);
+var gallonsIntoLitresConversionRate = 4.54609;
+var numberOfLitresUsed = numberOfGallonsUsed * gallonsIntoLitresConversionRate;
+var fuelPriceIntoPounds = (price / 100);
+var fuelCost = (numberOfLitresUsed * fuelPriceIntoPounds);
+
+var roundedCost = round(fuelCost,2);
+alert("This journey has cost \u00A3" + roundedCost+".");
+
+// miles is distance, milesPerGallon is MPG and price is Price per Litre of fuel
+}
