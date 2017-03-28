c7d5c4bc402721888f330275f10cfb582e0fc074
benedwards23 <benjamin.edwards@jato.com>
2017-03-28 16:55:38 +0100

    grabbed textbox with JS

--- myScript.js
+++ myScript.js
@@ -1,3 +1,9 @@
-function calculateFunction() {
-   alert('This function works.')
+function getValues() {
+  var theForm = document.forms["JourneyCost"];
+  var miles = theForm.elements["Distance"];
+  var numberOfMiles = 0;
+  if (miles.value!=""){
+  	numberOfMiles = parseInt(miles.value);
+  }
+  alert("You entered " + numberOfMiles + " into the calculator.");
    }
